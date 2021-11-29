ad0f4ae5bf3d9be8409a9c8954ce0db523e5fce3
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2021-11-29 23:00:25 +0100

    Task 1 temp ready to push

--- index.php
+++ index.php
@@ -1,49 +1,47 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>PG3: Tienda</title>
+		<title>YourOUTfit: Tienda online</title>
 		<meta charset="UTF-8" 
 		<script type="text/javascript" src="index.js"></script>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<h1>Tienda de ropa</h1>
+        <div id="header"><img src="images/YourOutfitLogo.png"> <!--</div>-->
+            <ol id = "lista_filtrado">
+                <li>Hombre</li>
+                <li>Mujer</li>
+                <li>Unisex</li>
+                <li>Accesorios</li>
+            </ol>
+        </div>
         <div class="busqueda">
             <form action=#>
                 <input type="text" placeholder="Busca..." name="barra_de_busqueda">
             </form>
         </div>
-        <div class="filtros">
-            <ol id = "lista_filtrado">
-                <li>filtro1</li>
-                <li>filtro2</li>
-                <li>filtro3</li>
-                <li>filtro4</li>
-                <li>filtro5</li>
-            </ol>
-        </div>
         <div class="content">
             <?php
-            if(file_exists('stock.xml')){
+            if (file_exists('stock.xml')) {
                 $stock = simplexml_load_file("stock.xml");
-
                 foreach ($stock->item as $item){
-                    $img_path = $item->imagen;
-                    $nombre = $item->nombre;
-                    $desc = $item->descripcion;
-                    $precio = $item->precio;?>
-
+                    ?>
                     <div class="item">
+                        <div id ="item_img" name="item_img"><img src=<?php echo $item->imagen?>></div>
+                        <div id="item_name" name="item_name"><?php echo $item->nombre ?> </div>
                         <?php
-                        $inner = '<div class="item_img"><img src="'.$img_path.'"></div>';
-                        $inner = $inner.'<div class="item_price">'.$precio.'€</div>';
-                        $inner = $inner.'<div class="item_name">'.$nombre.'</div>';
-                        $inner = $inner.'<div class="item_desc">'.$desc.'</div>';
-                        echo $inner;
+                        if($item->descuento == 0){
+                           ?><div id="item_price" name="item_price"><?php echo $item->precio.'€'?> <?php
+                        }
+                        else{
+                            ?>
+                            <div id="item_price_d" name="item_disc"><?php echo $item->precio.'€'?></div><div id="item_disc"><?php echo $item->precio*(1-($item->descuento/100))?>€</div>
+                            <div id="item_disc_tag">Descuento del <?php echo $item->descuento?>%</div>
+                           <?php
+                        }
                         ?>
                     </div>
-
-                <?php
+                    <?php
                 }
             }
             ?>
