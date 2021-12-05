b322093e3b66d8e2534bf925ff1199ff5d1df46c
ferchup <fersenior7@gmail.com>
2021-12-05 21:04:34 +0100

    Función Añadir reseña hecha

--- ajaxfuncts.php
+++ ajaxfuncts.php
@@ -1,7 +1,20 @@
 <?php
-
-
-
-
+if(isset($_GET['KEY']))
+    switch ($_GET['KEY']) {
+        case "resenya":
+            echo '<div id="form_nuevocoment" >
+                    <form  action="compra.php" >
+                        Nombre: <input type="text" name="nombre">
+                        Puntuación: <input type="number" onkeydown="return false;" value="0" min="0" max="5" name="puntuacion"> <br>
+                        Título: <input type="text" name="titulo"> <br>
+                        Comentario: <br>
+                        <textarea name="comentario" rows="5" cols="100"></textarea><br>
+                        <input type="submit" onclick="return ValidarForm(this.form);">
+                        <input type="hidden" name="compra_id" value="'.$_GET['compra_id'].'">
+                        <input type="hidden" name="subir" value="o">
+                    </form>
+                </div>';
+            break;
+    }
 
 ?>
--- compra.js
+++ compra.js
@@ -2,3 +2,46 @@ function funcionprecio(precio){
     var num = document.getElementById("cuantos").value;
     document.getElementById("precio_actual").innerHTML= "Total: "+parseInt(precio*num*100)/100+"€";
 }
+
+function funcion_resenya(item){
+    var xhr;
+    if(XMLHttpRequest)
+        xhr = new XMLHttpRequest();
+    else
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open('GET','ajaxfuncts.php?KEY=resenya&compra_id=' + item,true);
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status == 200){
+            document.getElementById("añadir_resenya").innerHTML = xhr.responseText;
+        }
+    }
+
+    xhr.send('');
+
+}
+
+function ValidarForm(f, id)
+{
+// Leer valores del formulario
+    var nombre = f.nombre.value;
+    var puntuacion = f.puntuacion.value;
+    var titulo = f.titulo.value;
+    var comentario = f.comentario.value;
+    var error = "";
+// Verificar que los campos obligatorios están rellenados
+    if(nombre=="")
+        error += "\tTu nombre es obligatorio!\n";
+    if(titulo=="")
+        error += "\tEl título es obligatorio\n";
+    if(comentario=="")
+        error += "\tNo has introducido ningún comentario!\n";
+// Si hay algún error, mostrar el mensaje
+    if(error != "")
+    {
+        alert("Validación del formulario:\n" + error);
+        return false;
+    }
+    else
+        return true;
+}
