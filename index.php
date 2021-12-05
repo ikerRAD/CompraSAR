6cb6a1a98ab944858c021ad4b4158d11a6093cd3
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2021-12-05 20:22:55 +0100

    Order implemented

--- index.php
+++ index.php
@@ -19,26 +19,25 @@
         </ol>
 
         <div class="busqueda">
+            Busca tus artículos favoritos:
             <form action=#>
-                <input type="text" placeholder="Busca..." name="barra_de_busqueda">
+                <input type="text" placeholder="Escribe..." name="barra_de_busqueda">
             </form>
 
-            <div class = "orden">Ordenar por:
+            <div class = "orden">
+                Ordenar por:
                 <ol id = "lista_ordenado">
-                    <li><a href="https://shangay.com/wp-content/uploads/2021/08/preview-26.jpg" class="ref-orden">A-Z</a></li>
-                    <li><a href="https://shangay.com/wp-content/uploads/2021/08/preview-26.jpg" class="ref-orden">Z-A</a></li>
-                    <li><a href="https://shangay.com/wp-content/uploads/2021/08/preview-26.jpg" class="ref-orden">Precio: mayor a menor</a></li>
-                    <li><a href="https://shangay.com/wp-content/uploads/2021/08/preview-26.jpg" class="ref-orden">Precio: menor a mayor</a></li>
-                    <li><a href="https://shangay.com/wp-content/uploads/2021/08/preview-26.jpg" class="ref-orden">Descuento: mayor a menor</a></li>
-                    <li><a href="https://shangay.com/wp-content/uploads/2021/08/preview-26.jpg" class="ref-orden">Descuento: menor a mayor</a></li>
+                    <li><a href="#" onclick=ordenar(1) class="ref-orden">A-Z</a></li>
+                    <li><a href="#" onclick=ordenar(2) class="ref-orden">Z-A</a></li>
+                    <li><a href="#" onclick=ordenar(3) class="ref-orden">Precio: mayor a menor</a></li>
+                    <li><a href="#" onclick=ordenar(4) class="ref-orden">Precio: menor a mayor</a></li>
                 </ol>
             </div>
 
         </div>
-        <div class="content">
+        <div id="content">
             <?php
             if (file_exists('stock.xml')) {
-                $e = 0;
                 $stock = simplexml_load_file("stock.xml");
                 foreach ($stock->item as $item){
                     ?>
@@ -48,11 +47,11 @@
                             <div class ="item_name"><?php echo $item->nombre;?></div><?php
 
                             if($item->descuento == 0){
-                               ?><div class ="item_price"><?php echo $item->precio.'€';?></div><?php
+                               ?><div class ="item_price"><span class="actual_price"><?php echo $item->precio;?></span>€</div><?php
                             }
                             else{
                                 ?>
-                                <div class ="item_price_d"><?php echo $item->precio.'€';?></div><div class ="item_disc"><?php echo round($item->precio*(1-($item->descuento/100)),2);?>€</div>
+                                <div class ="item_price_d"><?php echo $item->precio.'€';?></div><div class ="item_disc"><span class="actual_price"><?php echo round($item->precio*(1-($item->descuento/100)),2);?></span> €</div>
                                 <div class ="item_disc_tag">Descuento del <?php echo $item->descuento;?>%</div>
                                <?php
                             }
