53059e420dc9f532bed5c4ff813bbb5a2d22a0b0
ikerRAD <63415322+ikerRAD@users.noreply.github.com>
2021-12-06 02:23:12 +0100

    compra implementada y mejorada

--- compra.js
+++ compra.js
@@ -15,10 +15,153 @@ function comprarAJAX(form, pos){
 
     xhr.onreadystatechange = function(){
         if(xhr.readyState == 4 && xhr.status == 200){
-            document.getElementById("compra_resultado").innerHTML = xhr.responseText;
+
+            if(xhr.responseText === "OK") {
+                document.getElementsByClassName("formulario_compra")[0].innerHTML = '<form action=#>\n' +
+                    '                    Escribe tu dirección:\n' +
+                    '                    <select name="país">\n' +
+                    '                            <option value="Espanya">España</option>\n' +
+                    '                            <option value="Francia">Francia</option>\n' +
+                    '                            <option value="Italia">Italia</option>\n' +
+                    '                    </select>\n' +
+                    '                    <br>\n' +
+                    '                    Escribe tu ciudad:\n' +
+                    '                    <input type="text" name="ciudad" placeholder="tu ciudad..."> \n' +
+                    '                    <br>\n' +
+                    '                    Código postal:'+
+                    '                    <input type="text" maxlength="7" name="cp" placeholder="código postal..."> \n' +
+                    '                    <br> \n' +
+                    '                    Dirección: \n' +
+                    '                    <input type="text" name="dir" placeholder="dirección..."> \n' +
+                    '                    <input type="button" name="boton" value="proseguir" onclick="enviar(this.form, '+pos+','+ form.cantidad.value+');">\n' +
+                    '                </form>' +
+                    '               <div id="envio_resultado"></div>' ;
+
+            }else{
+                document.getElementById("compra_resultado").innerHTML = xhr.responseText;
+            }
         }
     }
 
     xhr.send('');
+
+}
+
+function enviar(f, pos, cantidad){
+
+    var mal = false;
+    if(f.ciudad.value.length<1){
+        alert("introduce una ciudad posible");
+        mal=true;
+    }
+    const pattern = /[0-9]+/;
+    if(f.cp.value.length<7 || !pattern.test(f.cp.value)){
+        alert("introduce un código postal válido");
+        mal=true;
+    }
+    if(f.dir.value.length<1){
+        alert("introduce una dirección válida");
+        mal=true;
+    }
+
+    if(!mal){
+        var xhr;
+        if(XMLHttpRequest)
+            xhr = new XMLHttpRequest();
+        else
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+
+        xhr.open('GET','ajaxfuncts.php?KEY=envio&ciudad='+f.ciudad.value+'&cp='+f.cp.value+'&dir='+f.dir.value,true);
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr.status == 200){
+                if(xhr.responseText==="OK"){
+                    document.getElementsByClassName("formulario_compra")[0].innerHTML = '<form action=#>\n' +
+                        'Propietario:'+
+                        '                    <input type="text" name="nombre" placeholder="Nombre Apellidos"> \n' +
+                        '                    <br>\n' +
+                        'Número de tarjeta:'+
+                        '                    <input type="text" name="number" maxlength="16" placeholder="xxxxxxxxxxxxxxxx" > \n' +
+                        '                    <br> \n' +
+                        'Válida hasta:'+
+                        '                    <input type="text" name="caducidad" maxlength="5" placeholder="MM/YY"> \n' +
+                        '                    <br> \n' +
+                        'Número de seguridad:'+
+                        '                    <input type="text" name="cvc" maxlength="3" placeholder="CVC"> \n' +
+                        '                    <br> \n' +
+                        '                    <input type="button" name="boton" value="finalizar" onclick="pagar(this.form, '+pos+','+ cantidad+');">\n' +
+                        '                </form>' ;
+                }else{
+                    document.getElementById("envio_resultado").innerHTML = xhr.responseText;
+                }
+            }
+        }
+
+        xhr.send('');
+    }
+
+}
+
+function pagar(f, pos, cantidad) {
+    const cardpattern = /[0-9]{16}/;
+    const cvcpattern = /[0-9]{3}/;
+    const datepattern = /[0-9]{1,2}\/[0-9]{2}/;
+    const name = /[a-zA-ZñÑáéíóúÁÉÍÓÚ]+(\s[a-zA-ZñÑáéíóúÁÉÍÓÚ\-]+.?)+/;
+    var mal = false;
+    if(f.nombre.value.length<3 || !name.test(f.nombre.value) ){
+        alert("nombre inválido");
+        mal = true;
+    }
+    var today = new Date();
+    if(!datepattern.test(f.caducidad.value) || parseInt(f.caducidad.value.split("/")[0])>12 || parseInt(f.caducidad.value.split("/")[1])<parseInt(today.getFullYear())%100 ||
+        (parseInt(f.caducidad.value.split("/")[1])==parseInt(today.getFullYear())%100 && parseInt(f.caducidad.value.split("/")[0])<(parseInt(today.getMonth()) + 1))){
+        alert("fecha inválida");
+        mal=true;
+    }
+    if(!cardpattern.test(f.number.value)){
+        alert("número de tarjeta inválido");
+        mal=true;
+    }
+    if(!cvcpattern.test(f.cvc.value)){
+        alert("número de seguridad inválido");
+        mal=true;
+    }
+
+    if(!mal){
+        var xhr;
+        if(XMLHttpRequest)
+            xhr = new XMLHttpRequest();
+        else
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+
+        xhr.open('GET','ajaxfuncts.php?KEY=pago&nombre='+f.nombre.value+'&date='+f.caducidad.value+'&tarjeta='+f.number.value+'&cvc='+f.cvc.value+'&pos='+pos+'&cant='+cantidad,true);
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr.status == 200){
+                if(xhr.responseText.split(" ")[0]==="ERROR:"){
+                    document.getElementsByClassName("formulario_compra")[0].innerHTML = '<form action=#>\n' +
+                        'Propietario:'+
+                        '                    <input type="text" name="nombre" placeholder="Nombre Apellidos"> \n' +
+                        '                    <br>\n' +
+                        'Número de tarjeta:'+
+                        '                    <input type="text" name="number" maxlength="16" placeholder="xxxxxxxxxxxxxxxx" > \n' +
+                        '                    <br> \n' +
+                        'Válida hasta:'+
+                        '                    <input type="text" name="caducidad" maxlength="5" placeholder="MM/YY"> \n' +
+                        '                    <br> \n' +
+                        'Número de seguridad:'+
+                        '                    <input type="text" name="cvc" maxlength="3" placeholder="CVC"> \n' +
+                        '                    <br> \n' +
+                        '                    <input type="button" name="boton" value="finalizar" onclick="pagar(this.form, '+pos+','+ cantidad+');">\n' +
+                        '                </form>' +
+                        '                <div class="error_pago">'+xhr.responseText.substring(7)+'</div>' ;
+                }else{
+                    document.getElementsByClassName("formulario_compra")[0].innerHTML = xhr.responseText;
+                }
+            }
+        }
+
+        xhr.send('');
+    }
 
 }
